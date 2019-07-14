4a78f42fc2b053d0e60e465fd59a594dc95ad7f6
Borja Sáez de Guinoa Vilaplana <borja.saez.de.guinoa.vilaplana@everis.com>
2019-07-14 10:17:34 +0200

    project init

---
+++ movieDBapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  movieDBapp
+//
+//  Created by Borja Saez de Guinoa Vilaplana on 14/07/2019.
+//  Copyright © 2019 Borja Saez de Guinoa Vilaplana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
