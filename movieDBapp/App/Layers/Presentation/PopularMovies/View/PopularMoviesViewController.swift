5cd9be6ff5ae07e763798a426e56ab0b1b7c2786
Borja Sáez de Guinoa Vilaplana <borja.saez.de.guinoa.vilaplana@everis.com>
2019-07-14 17:48:13 +0200

    added TableView and search bar to PopularMoviesVC

--- movieDBapp/App/Layers/Presentation/PopularMovies/View/PopularMoviesViewController.swift
+++ movieDBapp/App/Layers/Presentation/PopularMovies/View/PopularMoviesViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class PopularMoviesViewController: BaseViewController, PopularMoviesViewProtocol {
 
+    //MARK: - Views
+    @IBOutlet weak var tableView: UITableView!
+    
     
     private var presenter : PopularMoviesPresenter?
     
@@ -19,15 +22,23 @@ class PopularMoviesViewController: BaseViewController, PopularMoviesViewProtocol
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func configureViews(){
+        tableView.dataSource = self
+        tableView.delegate = self
     }
-    */
+    
+    
+    
+}
 
+extension PopularMoviesViewController : UITableViewDataSource, UITableViewDelegate{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
 }
